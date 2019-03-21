4226a401551b21d0f68f2cdcf3f58ecae1e305fd
DittoSoftware <42523546+DittoSoftware@users.noreply.github.com>
2019-03-20 22:22:25 -0400

    Add files via upload

---
+++ application/views/user_list.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Account Manager</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
+	<link href="https://d26b395fwzu5fz.cloudfront.net/keen-dataviz-1.1.3.css" rel="stylesheet" />
+  	<script src="https://d26b395fwzu5fz.cloudfront.net/keen-dataviz-1.1.3.js" type="text/javascript"></script> 
+</head>
+<body class="keen-dashboard" style="padding-top: 80px;">
+
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="navbar-brand">SIMS &raquo; Account Manager</a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-left">
+		  <li><a href="location.href='main'">Dashboard</a></li>
+		  <li><a href="location.href='filemanager'">File Manager</a></li>
+		  <li><a href="location.href='loadvw'">File Merge</a></li>
+        </ul>
+	  </div>
+    </div>
+  </div>
+<div class="container">
+	<h1 class="page-header text-center">Account Manager</h1>
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<a href="<?php echo base_url(); ?>index.php/users/addnew" class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Add New</a><br><br>
+			<table class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Username</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+						<th>User Type</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					foreach($users as $user){
+						?>
+						<tr>
+							<td><?php echo $user->id; ?></td>
+							<td><?php echo $user->username; ?></td>
+							<td><?php echo $user->fname; ?></td>
+							<td><?php echo $user->lname; ?></td>
+							<td><?php echo $user->email; ?></td>
+							<td><?php echo $user->role; ?></td>
+							<td><a href="<?php echo base_url(); ?>index.php/users/edit/<?php echo $user->id; ?>" class="btn btn-success"><span class="glyphicon glyphicon-edit"></span> Edit</a> || <a href="<?php echo base_url(); ?>index.php/users/delete/<?php echo $user->id; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Delete</a></td>
+						</tr>
+						<?php
+					}
+					?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+</body>
+</html>
