d0f75c576f271eda3f4b781e4dbda837c8ebb4f3
DittoSoftware <42523546+DittoSoftware@users.noreply.github.com>
2019-03-25 19:33:00 -0400

    More Image Links Updated

--- application/views/signin_view.php
+++ application/views/signin_view.php
@@ -49,23 +49,23 @@ if(isset($_POST['username'])){
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
            <!-- <link rel="stylesheet" type="text/css" href="http://localhost/simsdemo/CSS/login.css"> -->
-            <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>CSS/login.css">
+            <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/CSS/login.css">
             
 
     </head>
 
 
-    <body background="http://localhost/simsdemo/images/loginBackground.jpg">
+    <body background="<?= base_url()?>/images/loginBackground.jpg">
 
     <div class="col-sm-2 sidenav">
                     <br><br><br>
-                    <img class="logo" src="http://localhost/simsdemo/images/textOnlyLogo.png" alt="Logo">
+                    <img class="logo" src="<?= base_url()?>/images/textOnlyLogo.png" alt="Logo">
                     <br>
                     <p class="quote">It's relationships, not programs, that change children</p>
                   </div>
 
         <div class="loginbox">
-         <img src="http://localhost/simsdemo/images/loginAvatar.jpg" class = "avatar"> 
+         <img src="<?= base_url()?>/images/loginAvatar.jpg" class = "avatar"> 
             <h1>Login Here:</h1>
             <form method="POST" action="#">
                 <p>Username</p>
