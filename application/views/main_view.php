05795237e6eea641504d69e112d811b3846b9b8c
FalcoZero <42524597+FalcoZero@users.noreply.github.com>
2019-03-26 17:32:52 -0400

    Update main_view.php

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -36,7 +36,6 @@ if(empty($_POST['s_id']))
   <script type="text/javascript" src="highcharts.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.3.2/holder.min.js" type="text/javascript"></script>
   <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
