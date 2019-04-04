b4f7ef23bf5ae47434cab05be7e0c04c248e0ed1
DittoSoftware <42523546+DittoSoftware@users.noreply.github.com>
2019-04-04 08:42:02 -0400

    Undoing the merge css
    Bootstrap for merge needs to go.

--- application/views/merge_view.php
+++ application/views/merge_view.php
@@ -15,31 +15,15 @@ if(!isset($_SESSION['username'])){
 <script src="<?php echo base_url(); ?>js/buttons.html5.min.js"></script>
 <script src="<?php echo base_url(); ?>js/jszip.min.js"></script> 
 
-<!--Cached Boostrap CDN-->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-    <!--Cached jQuery CDN-->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-
-
-
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/stylesheet.css">
-
-
-<!--
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
 	<link href="https://d26b395fwzu5fz.cloudfront.net/keen-dataviz-1.1.3.css" rel="stylesheet" />
   	<script src="https://d26b395fwzu5fz.cloudfront.net/keen-dataviz-1.1.3.js" type="text/javascript"></script> 
 
 <link rel="stylesheet" href="<?php echo base_url();?>CSS/buttons.dataTables.min.css">
 <link rel="stylesheet" href="<?php echo base_url();?>CSS/jquery.dataTables.min.css">
--->
-
 </head>
-<body>
-  <!--
+<body class="keen-dashboard" style="padding-top: 80px;">
+
   <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container-fluid">
       <div class="navbar-header">
@@ -63,6 +47,7 @@ if(!isset($_SESSION['username'])){
 	  </div>
     </div>
   </div>
+<<<<<<< HEAD
 -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
 
@@ -91,6 +76,8 @@ if(!isset($_SESSION['username'])){
 				</div>
 		</nav>
 
+=======
+>>>>>>> parent of 5149268... file merge nav
 <center><h1>Export Data</h1></center>
 <table id="example" class="display" style="width:100%">
         <thead>
