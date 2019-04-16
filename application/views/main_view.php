730c9de710dd1bafa2043e9a4c4cca3d0a2bde9e
FalcoZero <42524597+FalcoZero@users.noreply.github.com>
2019-04-16 17:03:22 -0400

    Update main_view.php

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -6,6 +6,7 @@
    if(empty($_POST['s_id']))
    {
      $_POST['s_id'] = "0";
+     echo $_POST['s_id'].name;
    }
    if(empty($_POST['p_id']))
    {
@@ -182,17 +183,7 @@
                         <!-- Doesn't Works -->
                      </select>
                      <br><br>
-                     <select name="p_id" style="width: 100%;">
-                     <option value="0" selected>All Programs</option>
-                     <option value="AFL">AFL Division</option>
-                     <option value="After_School">Afterschool Division</option>
-                     <option value="AmeriCorps">AmeriCorps (Literacy) Division</option>
-                     <option value="BTS">Bridge to Sucess Division</option>
-                     <option value="Gear_Up">Gear Up</option>
-                     <option value="SEP">Student Enrichment Division</option>
-                     </select>
                      <?php echo "Currently Displaying: School #".$_POST['s_id']."'s Student Data" ; ?>
-                      Currently Displaying: <p id="demo"></p> Student Data
                      <button type="submit">Update Charts</button>
 </div>
                     
@@ -284,6 +275,31 @@
                                  include 'ChartView.php'; ?>
                               </div>
                               </div>
+                              <!--Card-->
+                              <div class="card mb-4 shadow p-3 mb-5 bg-white rounded">
+                           <div class="card-body center">
+                              <h4 class="card-title text-center">Program(s)</h4>
+                              <div id="piechart4"></div>
+                              <?php $field = "piechart4"; 
+                                 $school = $_POST['s_id']; 
+                                 $fieldArray = fieldCount('Gender_Code', 'tbl_name',$school);
+                                 $arrayA = [];
+                                 $arrayB = [];
+                                 $i = 1;
+                                 while($i<count($fieldArray))
+                                 {
+                                 array_push($arrayA,$fieldArray[$i]["Gender_Code"]);
+                                 $i++;
+                                 } 
+                                 $i = 1;
+                                 while($i<count($fieldArray))
+                                 {
+                                 array_push($arrayB,$fieldArray[$i]['count']);
+                                 $i++;
+                                 } 
+                                 include 'ChartView.php'; ?>
+                              </div>
+                              </div>
                               
                               
                               
@@ -294,12 +310,6 @@
                               </div>
                               </div>
                
-         <script language = "JavaScript">
-         function myFunction() {
-         var x = document.getElementsByName("p_id")[0].value;
-         document.getElementById("demo").innerHTML = x;
-         }
-</script>
       </form>
    </body>
 </html>
