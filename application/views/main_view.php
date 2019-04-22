fa8832276cb048b9bcf49c4b8ab653197bcb57fa
DittoSoftware <42523546+DittoSoftware@users.noreply.github.com>
2019-04-22 16:52:04 -0400

    Update main_view.php

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -188,7 +188,7 @@
                         <option value="N998">Home Education Setting</option>
                         <!-- Doesn't Works -->
                      </select>
-                     <br><br>
+                     
                      <?php echo "Currently Displaying: ".$_POST['s_id']."'s Student Data" ; ?>
                      <br>
                      <button type="submit">Update Charts</button><br><br>
